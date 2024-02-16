b642db6666183ca798976f1b9254c8c5c801209a
André Prado Junior <116444319+Andrepjr@users.noreply.github.com>
2024-02-16 17:40:32 -0300

    Navbar e Footer

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './paginas/home/Home'
+import Login from './paginas/login/Login'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+    return (
+        <>
+            <BrowserRouter>
+            <Navbar />
+            <div className='min-h-[80vh]'>
+                <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/home" element={<Home />} />
+                </Routes>
+            </div>
+            <Footer />
+        </BrowserRouter >
+
+        </>
+    );
+}
+
+export default App;
