e6c2aa514a4b353b15fa3270955fffd0c36db809
André Prado Junior <116444319+Andrepjr@users.noreply.github.com>
2024-02-21 12:49:52 -0300

    Temas

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,19 +1,34 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL:'https://blogpessoal-1t74.onrender.com'
+  baseURL: 'https://blogdothiagofaccipieri.onrender.com/'
 })
 
-//promessas -  assincrona para o cadastro do usuario
-export const cadastrarUsuario = async(url:string, dados: Object, setDados: Function)=>{
-    
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data)
-
+export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.post(url, dados)
+  setDados(resposta.data)
 }
 
-//promessa assincrona para o login do usuario
-export const login = async(url: string, dados: Object, setDados: Function)=>{
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data)
+export const login = async(url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.post(url, dados)
+  setDados(resposta.data)
+}
+
+export const buscar = async(url: string, setDados: Function, header: Object) => {
+  const resposta = await api.get(url, header)
+  setDados(resposta.data)
+}
+
+export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.post(url, dados, header)
+  setDados(resposta.data)
+}
+
+export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
+  const resposta = await api.put(url, dados, header)
+  setDados(resposta.data)
+}
+
+export const deletar = async(url: string, header: Object) => {
+  await api.delete(url, header)
 }
